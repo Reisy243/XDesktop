b578cb32f0468d1b137a4c0250899ecf5f92f1c5
Isaac David Orozco Delgado (Reisy RedPanda) <reisimineredpanda@gmail.com>
2025-07-30 21:46:04 -0600

    Reaciendo

--- io/github/reisy243/XDesktop/XDFM/Forms/FormExplorer.java
+++ io/github/reisy243/XDesktop/XDFM/Forms/FormExplorer.java
@@ -1,35 +1,20 @@
 package io.github.reisy243.XDesktop.XDFM.Forms;
 
 import javax.swing.*;
-<<<<<<< HEAD
 import javax.swing.tree.DefaultMutableTreeNode;
-=======
->>>>>>> fb73bd4d0d1fabdb9b1654f82ac996d8f89f1868
 
 public class FormExplorer extends JFrame {
 	public FormExplorer() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("XDFile-Manager");
 
-<<<<<<< HEAD
-		JSplitPane MainPanel = new JSplitPane();
-		JTable Files = new JTable();
-
 		DefaultMutableTreeNode root = new DefaultMutableTreeNode("Root: /");
-		DefaultMutableTreeNode FolderBin = new DefaultMutableTreeNode("bin");
-		DefaultMutableTreeNode FolderSBin = new DefaultMutableTreeNode("sbin");
-		DefaultMutableTreeNode FolderLib = new DefaultMutableTreeNode("lib");
-		DefaultMutableTreeNode FolderLib64 = new DefaultMutableTreeNode("lib64");
-		DefaultMutableTreeNode FolderUsr = new DefaultMutableTreeNode("usr");
-
-		root.add(FolderBin);
-		root.add(FolderSBin);
-		root.add(FolderLib);
-		root.add(FolderLib64);
-		root.add(FolderUsr);
-		JTree FileTree = new JTree(root);
 
 		JMenuBar MenuBar = new JMenuBar();
+		JSplitPane MainPanel = new JSplitPane();
+		JTree FileTree = new JTree(root);
+		JTable Files = new JTable();
+
 		JMenu MenuFile = new JMenu("File");
 		JMenu MenuEdit = new JMenu("Edit");
 		JMenu MenuView = new JMenu("View");
@@ -45,6 +30,16 @@ public class FormExplorer extends JFrame {
 
 		JMenuItem MIShowHid = new JMenu("Show hiden files");
 
+		DefaultMutableTreeNode FolderBin = new DefaultMutableTreeNode("bin");
+		DefaultMutableTreeNode FolderSBin = new DefaultMutableTreeNode("sbin");
+		DefaultMutableTreeNode FolderLib = new DefaultMutableTreeNode("lib");
+		DefaultMutableTreeNode FolderLib64 = new DefaultMutableTreeNode("lib64");
+		DefaultMutableTreeNode FolderUsr = new DefaultMutableTreeNode("usr");
+
+		MenuBar.add(MenuFile);
+		MenuBar.add(MenuEdit);
+		MenuBar.add(MenuView);
+
 		MenuFile.add(MINewDir);
 		MenuFile.add(MINewFile);
 		MenuFile.add(MIExit);
@@ -56,18 +51,18 @@ public class FormExplorer extends JFrame {
 
 		MenuView.add(MIShowHid);
 
-		MenuBar.add(MenuFile);
-		MenuBar.add(MenuEdit);
-		MenuBar.add(MenuView);
+		root.add(FolderBin);
+		root.add(FolderSBin);
+		root.add(FolderLib);
+		root.add(FolderLib64);
+		root.add(FolderUsr);
 
 		MainPanel.setLeftComponent(new JScrollPane(FileTree));
 		MainPanel.setRightComponent(new JScrollPane(Files));
 
-		setJMenuBar(MenuBar);
-=======
-		JPanel MainPanel = new JPanel();
+		MIExit.addActionListener(e -> System.exit(0));
 
->>>>>>> fb73bd4d0d1fabdb9b1654f82ac996d8f89f1868
+		setJMenuBar(MenuBar);
 		setContentPane(MainPanel);
 		setSize(800,600);
 	}
